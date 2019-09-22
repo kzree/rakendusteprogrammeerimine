3e1ba62628646bf79834b44df238ccc31065c025
Erkki Sula <43129445+erkkisula@users.noreply.github.com>
2019-09-22 17:06:53 +0300

    React jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,6 @@ import ReactDOM from "react-dom";
 const root = document.getElementById("app");
 
 ReactDOM.render(
-    React.createElement(
-        "button",
-        {},
-        "I am a buton, Hello World!"
-    ),
+    <button>I am button</button>,
     root
 )
