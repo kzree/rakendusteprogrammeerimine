b57adf8371b6e89db60c47a53e9ceee95ed25ff9
Erkki Sula <43129445+erkkisula@users.noreply.github.com>
2019-11-16 15:22:47 +0200

    HOC

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,34 +3,35 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { userIcon, cartIcon } from "../Icons";
 import css from "./header.css";
-import { AuthContext } from "../index.jsx";
-const Header = () => {
-    return (
-        <AuthContext.Consumer>
-            {
-                (contextValue) => (
-                    <div className="header">
-                        <Link to={"/"}>
-                            <img className="logo" src="/img/logo.png" alt="Logo"></img>
-                        </Link>
-                        <div className="buttons">
-                            <Link to={"/login"}>
-                                <div className="header-icon">
-                                    <img src={userIcon} className="header-icon-img" alt="user_icon" />
-                                </div>
-                            </Link>
-                            <div className="divider"></div>
-                            <Link to={"/"}>
-                                <div className="header-icon">
-                                    <img src={cartIcon} className="header-icon-img" alt="cart_icon" />
-                                </div>
-                            </Link>
-                        </div>
+import PropTypes from "prop-types";
+import authConsumer from "./authConsumer.jsx";
+
+const Header = ({user}) => {
+    return(
+        <div className="header">
+            <Link to={"/"}>
+                <img className="logo" src="/img/logo.png" alt="Logo"></img>
+            </Link>
+            <div className="buttons">
+                <Link to={"/login"}>
+                    <div className="header-icon">
+                        <img src = {userIcon} className = "header-icon-img" alt="user_icon"/>
                     </div>
-                )
-            }
-        </AuthContext.Consumer>
+                </Link>
+                <div className="divider"></div>
+                <Link to={"/"}>                
+                    <div className="header-icon">
+                            <img src={cartIcon} className = "header-icon-img" alt="cart_icon"/>
+                    </div>
+                </Link>
+            </div>
+        </div>
     )
 };
 
-export default Header;
+Header.propTypes = {
+    token: PropTypes.string,
+    user: PropTypes.object,
+};
+
+export default authConsumer(Header);
---
+++ src/components/authConsumer.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { AuthContext } from "../index.jsx";
+
+const authConsumer = (WrappedComponent) => {
+    return class extends React.PureComponent{
+        static displayName = "authconsumer-hoc";
+        render() {
+            return (
+                <AuthContext.Consumer>
+                    {
+                        (value) => <WrappedComponent {...this.props} {...value}/>
+                    }
+                </AuthContext.Consumer>
+            );
+        }
+    };
+};
+
+export default authConsumer;
