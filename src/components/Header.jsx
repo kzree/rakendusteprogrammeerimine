494d69ae3b286f3a944f55c69bf2304386a042b1
Erkki Sula <43129445+erkkisula@users.noreply.github.com>
2019-11-23 01:46:26 +0200

    Header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,26 +2,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { userIcon, cartIcon } from "../Icons";
-import css from "./header.css";
+import "./header.css";
 import PropTypes from "prop-types";
 import authConsumer from "./authConsumer.jsx";
 
-const Header = ({user}) => {
-    return(
+const Header = ({token, user }) => {
+    console.log("header", token, user);
+    return (
         <div className="header">
             <Link to={"/"}>
                 <img className="logo" src="/img/logo.png" alt="Logo"></img>
             </Link>
             <div className="buttons">
-                <Link to={"/login"}>
-                    <div className="header-icon">
-                        <img src = {userIcon} className = "header-icon-img" alt="user_icon"/>
-                    </div>
-                </Link>
+                {user.email && <ProfileIcon user={user} />}
+                {!user.email && <LoginRegisterIcon />}
                 <div className="divider"></div>
-                <Link to={"/cart"}>                
+                <Link to={"/cart"}>
                     <div className="header-icon">
-                            <img src={cartIcon} className = "header-icon-img" alt="cart_icon"/>
+                        <img src={cartIcon} className="header-icon-img" alt="cart_icon" />
                     </div>
                 </Link>
             </div>
@@ -33,5 +31,21 @@ Header.propTypes = {
     token: PropTypes.string,
     user: PropTypes.object,
 };
+
+const LoginRegisterIcon = () => (
+    <Link className={"header-icon"} to={"/login"}>
+        <img src={userIcon} className="header-icon-img" alt="cart_icon"/>
+    </Link>
+);
+
+const ProfileIcon = ({ user }) => (
+    <Link className={"header-icon"} to={`/users/${user._id}`}>
+        <img src={userIcon} className="header-icon-img" alt="cart_icon"/>
+    </Link>
+);
+
+ProfileIcon.propTypes = {
+    user: PropTypes.object.isRequired
+  };
 
 export default authConsumer(Header);
