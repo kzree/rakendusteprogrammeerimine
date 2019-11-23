8ece8908c347e09f0ac9c8543c7c413bc366ea3e
Erkki Sula <43129445+erkkisula@users.noreply.github.com>
2019-11-23 02:32:29 +0200

    Not found css

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,27 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
+import { Link } from "react-router-dom";
+import "../style/NotFound.css";
 
 class NotFound extends React.PureComponent {
     render(){
         return (
             <div>
-                Not Found
+                <div className="notf-wrapper">
+                    <div className="notf-content">
+                        <div className="notf-title">Did you know?</div>
+                        <div className="notf-secondary">There are three things in this world that don't exist</div>
+                        <div className="notf-spacer"></div>
+                        <div className="notf-bullets">
+                            <div className="notf-bullet">Dragons</div>
+                            <div className="notf-bullet">Free meals</div>
+                            <div className="notf-bullet">This page</div>
+                        </div>
+                        <Link to={"/"}>
+                            <div className="notf-button">Back to the store</div>
+                        </ Link>
+                    </div>
+                </div>
             </div>
         )
     }
